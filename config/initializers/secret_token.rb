169c96b667c360151833847719a0f4217e5fd5ce
Hasiba <munlite138@gmail.com>
2014-01-08 23:49:49 -0500

    Initialized Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlickrCrit::Application.config.secret_key_base = '7ce8b9ab34fc5ade2c2f66991452b47cac2f083de64ff83119ecf0938d105a1cb9beab9857d1cc4c91c38bb4ca2494f9a044bbf3ee00c9bfdf7860b0efcce956'
